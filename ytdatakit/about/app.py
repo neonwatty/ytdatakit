51f5cb47e68be5b80a01a624ca878942ffec0099
Jeremy Watt <jermwatt@gmail.com>
2024-08-04 16:15:17 -0700

    about page added

---
+++ ytdatakit/about/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+def app():
+    st.markdown(
+        "### YTDatakit \n\n"
+        "**Download YouTube videos, transcripts, thumbnails, and channel data - all in one place.** \n\n"
+        "One app per tab - as detailed below. \n\n"
+        "**Tab 1:**  💡 About - you are here. \n\n"
+        "**Tab 2:**  🎞️ Video downloader - enter a YouTube / Shorts url and download its mp4 file. \n\n"
+        "**Tab 3:**  📜 Transcript downloader - download multiple YouTube / Shorts transcripts at once. \n\n"
+        "**Tab 4:**  📌 Thumbnail downloader - download multiple YouTube / Shorts thumbnails at once. \n\n"
+        "**Tab 5:**  📕 Channel downloader - download all YouTube video ids associated with a channel name. \n\n"
+        ""
+        "Each app is illustrated in the gif below. \n \n"
+        "![Alt Text](https://github.com/neonwatty/readme_gifs/blob/main/ytdatakit.gif?raw=true)"
+    )
--- ytdatakit/app.py
+++ ytdatakit/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from ytdatakit.about.app import app as about_page
 from ytdatakit.youtube_downloader.app import app as video_downloader
 from ytdatakit.youtube_transcript_downloader.app import app as transcript_downloader
 from ytdatakit.youtube_thumbnail_downloader.app import app as thumbnail_downloader
@@ -8,15 +9,17 @@ app_name = "ytdatakit"
 st.set_page_config(page_title=app_name)
 st.title(app_name)
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(
-    ["Video downloader", "Transcript downloader", "Thumbnail downloader", "Channel downloader", "💡 About", "Settings"]
+tab1, tab2, tab3, tab4, tab5 = st.tabs(
+    ["💡 About", "🎞️ Video downloader", "📜 Transcript downloader", "📌 Thumbnail downloader", "📕 Channel downloader"]
 )
 
 with tab1:
-    video_downloader()
+    about_page()
 with tab2:
-    transcript_downloader()
+    video_downloader()
 with tab3:
-    thumbnail_downloader()
+    transcript_downloader()
 with tab4:
+    thumbnail_downloader()
+with tab5:
     channel_downloader()
